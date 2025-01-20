114fd0aa4c3018aeea62452b74743ea0589ac7dd
heroinsFormula <143944003+heroinsFormula@users.noreply.github.com>
2025-01-20 21:21:09 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  base: '/odskrtavac-realnew/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
